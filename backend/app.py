1a4b8e7c12aa8349309a0334d9c44d2c5e6c3a80
Ighodaro John <johndaro222@gmail.com>
2026-06-24 13:08:10 +0100

    Work in Jesus name

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,13 @@ import certifi
 import os
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:56652", "https://johndaro7.github.io"])
+CORS(app,
+     origins=[
+         "https://johndaro7.github.io",
+         r"http://localhost:\d+",
+         r"http://127\.0\.0\.1:\d+",
+     ],
+     supports_credentials=True)
 
 client = MongoClient(
     'mongodb+srv://JOHNDARO:exmartial2003@cluster0.5nwpjbf.mongodb.net/?appName=Cluster0',
